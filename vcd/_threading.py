d1989dcfd9f6a1f8668289897c68747b36a890cd
sralloza <sralloza@gmail.com>
2019-05-26 16:36:29 +0200

    Deleted Worker. Added async function run(), which substitutes Worker

--- vcd/_threading.py
+++ vcd/_threading.py
@@ -1,120 +1,35 @@
 """Multithreading workers for the vcd."""
 
 import logging
-import time
-from queue import Queue
-from threading import Thread
 
 from ._requests import DownloaderError
 from .links import BaseLink
 from .subject import Subject
-from .time_operations import seconds_to_str
+
+logger = logging.getLogger(__name__)
 
 
-class Worker(Thread):
-    """Special worker for vcd multithreading."""
+async def run(anything):
+    """Runs the thread"""
+    if isinstance(anything, BaseLink):
+        logger.debug('Found Link %r, processing', anything.name)
+        try:
+            await anything.download()
+        except FileNotFoundError as ex:
+            logger.exception('FileNotFoundError in url %s (%r)', anything.url, ex)
+        except DownloaderError as ex:
+            logger.exception('DownloaderError in url %s (%r)', anything.url, ex)
 
-    def __init__(self, queue, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        logger.info('Completed work of Link %r', anything.name)
 
-        self.logger = logging.getLogger(__name__)
-        self.queue: Queue = queue
-        self.status = 'idle'
-        self.timestamp = None
-        self.current_object = None
+    elif isinstance(anything, Subject):
+        logger.debug('Found Subject %r, processing', anything.name)
+        try:
+            await anything.find_links()
+        except DownloaderError as ex:
+            logger.exception('DownloaderError in subject %s (%r)', anything.name, ex)
 
-    def to_log(self, integer=False):
-        color = 'black'
-        exec_time = 0
-
-        if self.timestamp is not None:
-            exec_time = time.time() - self.timestamp
-
-            if exec_time < 30:
-                color = 'green'
-            elif 30 < exec_time < 60:
-                color = "orange"
-            else:
-                color = "red"
-
-        status = f'<font color="{color}">{self.name}: {self.status} - '
-
-        if exec_time > 90:
-            status += f'[{seconds_to_str(exec_time, integer=integer)}] '
-
-        if isinstance(self.current_object, BaseLink):
-            status += f'{self.current_object.subject.name} → {self.current_object.name}'
-        elif isinstance(self.current_object, Subject):
-            status += f'{self.current_object.name}'
-        else:
-            status += 'None'
-
-        status += '</font>'
-
-        return status
-
-    # noinspection PyUnresolvedReferences
-    def run(self):
-        """Runs the thread"""
-        while True:
-            self.status = 'idle'
-            self.logger.info('Worker %r ready to continue working', self.name)
-            anything = self.queue.get()
-            self.timestamp = time.time()
-            self.logger.debug('%d items left in queue (%d unfinished tasks)', self.queue.qsize(),
-                              self.queue.unfinished_tasks)
-
-            self.status = 'working'
-            self.current_object = anything
-
-            if isinstance(anything, BaseLink):
-                self.logger.debug('Found Link %r, processing', anything.name)
-                try:
-                    anything.download()
-                except FileNotFoundError as ex:
-                    self.logger.exception('FileNotFoundError in url %s (%r)', anything.url, ex)
-                except DownloaderError as ex:
-                    self.logger.exception('DownloaderError in url %s (%r)', anything.url, ex)
-
-                self.logger.info('Worker %r completed work of Link %r', self.name, anything.name)
-                self.queue.task_done()
-
-            elif isinstance(anything, Subject):
-                self.logger.debug('Found Subject %r, processing', anything.name)
-                try:
-                    anything.find_links()
-                except DownloaderError as ex:
-                    self.logger.exception('DownloaderError in subject %s (%r)', anything.name, ex)
-
-                self.logger.info('Worker %r completed work of Subject %r', self.name, anything.name)
-                self.queue.task_done()
-            elif anything is None:
-                self.logger.info('Closing thread, received None')
-                self.logger.info('%d unfinished tasks', self.queue.unfinished_tasks)
-                self.current_object = None
-                self.timestamp = None
-                return
-
-            self.logger.info('%d unfinished tasks', self.queue.unfinished_tasks)
-            self.current_object = None
-            self.timestamp = None
-
-
-def start_workers(queue, nthreads=20):
-    """Starts the wokers.
-
-    Args:
-        queue (Queue): queue to manage the workers's tasks.
-        nthreads (int): number of trheads to start.
-
-    Returns:
-
-    """
-    thread_list = []
-    for i in range(nthreads):
-        thread = Worker(queue, name=f'W-{i + 1:02d}', daemon=True)
-        thread.logger.debug('Started worker named %r', thread.name)
-        thread.start()
-        thread_list.append(thread)
-
-    return thread_list
+        logger.info('Completed work of Subject %r', anything.name)
+    elif anything is None:
+        logger.info('Closing thread, received None')
+        return

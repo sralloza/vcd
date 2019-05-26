10d9e28e202de7f4376448123f4b4feb72893121
sralloza <sralloza@gmail.com>
2019-05-26 16:48:42 +0200

    Asynced Subject

--- vcd/subject.py
+++ vcd/subject.py
@@ -9,23 +9,21 @@ from bs4 import BeautifulSoup
 from requests import Response
 
 from .options import Options
+
 from vcd.alias import Alias
 from vcd.links import BaseLink, Resource, Delivery, Forum, Folder
 
 
-# pylint: disable=too-many-instance-attributes
-
 class Subject:
     """Representation of a subject."""
 
-    def __init__(self, name, url, downloader, queue):
+    def __init__(self, name, url, downloader):
         """
 
         Args:
             name (str): name of the subject.
             url (str): url of the subject.
             downloader (Downloader): downloader to download files.
-            queue (Queue): queue to controll threads.
         """
 
         if ' ' in name:
@@ -34,7 +32,6 @@ class Subject:
         self.name = Alias.real_to_alias(sha1(url.encode()).hexdigest(), name)
         self.url = url
         self.downloader = downloader
-        self.queue = queue
 
         self.response: Response = None
         self.soup: BeautifulSoup = None
@@ -54,13 +51,13 @@ class Subject:
     def __str__(self):
         return f'{self.name}'
 
-    def make_request(self):
+    async def make_request(self):
         """Makes the primary request."""
         self.logger.debug('Making subject request')
-        self.response = self.downloader.get(self.url)
-        self.soup = BeautifulSoup(self.response.text, 'html.parser')
+        self.response = await self.downloader.get(self.url)
+        self.soup = BeautifulSoup(await self.response.text(), 'html.parser')
 
-        self.logger.debug('Response obtained [%d]', self.response.status_code)
+        self.logger.debug('Response obtained [%d]', self.response.status)
         self.logger.debug('Response parsed')
 
     def create_folder(self):
@@ -79,21 +76,10 @@ class Subject:
         self.logger.debug('Adding url: %s', url.name)
         self.notes_links.append(url)
 
-    def download_notes(self):
-        """Downloads the notes multithreadingly."""
-        self.logger.debug('Downloading notes by multithread: %s', self.name)
-        if self.queue is None:
-            self.logger.critical('Queue is not defined')
-            raise RuntimeError('Queue is not defined')
-
-        for link in self.notes_links:
-            self.logger.debug('Adding link to queue: %r', link.name)
-            self.queue.put(link)
-
-    def find_links(self):
+    async def find_links(self):
         """Finds the links downloading the primary page."""
         self.logger.debug('Finding links of %s', self.name)
-        self.make_request()
+        await self.make_request()
 
         _ = [x.extract() for x in self.soup.findAll('span', {'class': 'accesshide'})]
         _ = [x.extract() for x in self.soup.findAll('div', {'class': 'mod-indent'})]
@@ -107,23 +93,18 @@ class Subject:
 
                 if 'resource' in url:
                     self.logger.debug('Created Resource (subject search): %r, %s', name, url)
-                    self.add_link(
-                        Resource(name, url, self, self.downloader, self.queue))
+                    self.add_link(Resource(name, url, self, self.downloader))
                 elif 'folder' in url:
                     self.logger.debug('Created Folder (subject search): %r, %s', name, url)
-                    self.add_link(
-                        Folder(name, url, self, self.downloader, self.queue))
+                    self.add_link(Folder(name, url, self, self.downloader))
                 elif 'forum' in url:
                     self.logger.debug('Created Forum (subject search): %r, %s', name, url)
-                    self.add_link(
-                        Forum(name, url, self, self.downloader, self.queue))
+                    self.add_link(Forum(name, url, self, self.downloader))
                 elif 'assign' in url:
                     self.logger.debug('Created Delivery (subject search): %r, %s', name, url)
-                    self.add_link(
-                        Delivery(name, url, self, self.downloader, self.queue))
+                    self.add_link(Delivery(name, url, self, self.downloader))
 
             except AttributeError:
                 pass
 
         self.logger.debug('Downloading files for subject %r', self.name)
-        self.download_notes()
